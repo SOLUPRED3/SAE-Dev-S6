2b3b1144c344d5fcdced49001ff56dd0dc2257e8
Julien_Druelle <julien.druelle007@gmail.com>
2024-02-06 13:09:32 +0100

    update role

--- bbl_articles/src/Controller/Admin/ReservationsCrudController.php
+++ bbl_articles/src/Controller/Admin/ReservationsCrudController.php
@@ -3,14 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Reservations;
-use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
-use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
-use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class ReservationsCrudController extends AbstractCrudController
 {
