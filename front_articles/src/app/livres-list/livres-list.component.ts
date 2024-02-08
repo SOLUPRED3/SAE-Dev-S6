ca246a685a5404ea12be0b38541fa20967d4424e
AdrielMarchant <Adriel.tahiti@gmail.com>
2024-02-08 16:19:43 +0100

    ajout recherche livre

--- front_articles/src/app/livres-list/livres-list.component.ts
+++ front_articles/src/app/livres-list/livres-list.component.ts
@@ -6,24 +6,50 @@ import { Auteur } from '../models/auteur';
 @Component({
   selector: 'app-livres-list',
   templateUrl: './livres-list.component.html',
-  styleUrl: './livres-list.component.css'
+  styleUrls: ['./livres-list.component.css']
 })
 export class LivresListComponent implements OnInit {
   livres: Livre[] = [];
   auteurs: Auteur[] = [];
+  filteredLivres: Livre[] = [];
+  searchQuery: string = '';
+
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
     this.apiService.getLivres().subscribe((data: Livre[]) => {
       this.livres = data;
+      this.applyFilter(); // Appliquer le filtre initialement
     });
     this.apiService.getAuteurs().subscribe((data: Auteur[]) => { 
       this.auteurs = data;
     });
   }
 
+  // Fonction de recherche de livre
+  applyFilter(): void {
+    if (!this.searchQuery) {
+      this.filteredLivres = this.livres;
+    } else {
+      this.filteredLivres = this.livres.filter(livre =>
+        livre.titre.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    }
+  }
+
+  // Fonction pour obtenir le nom de l'auteur
   getAuteurName(auteurId: number): string {
-    const auteur = this.auteurs.find(a => a.id === auteurId); // Find author by ID
-    return auteur ? `${auteur.nom} ${auteur.prenom}` : ''; // Return full name if found, otherwise return empty string
+    const auteur = this.auteurs.find(a => a.id === auteurId);
+    return auteur ? `${auteur.nom} ${auteur.prenom}` : '';
+  }
+
+  // Fonction appelée lorsqu'une sélection est effectuée dans la recherche
+  onSearchChange(): void {
+    this.applyFilter();
+  }
+
+  // Fonction appelée lorsqu'un livre est sélectionné
+  onLivreSelected(): void {
+    this.applyFilter();
   }
 }
